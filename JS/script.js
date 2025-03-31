b010d9d98a9b519db61f98cde31d4d73ffd6ff44
Patrick <gcc-flagday@gmail.com>
2025-03-31 22:41:39 +0100

    Major Improvements

--- JS/script.js
+++ JS/script.js
@@ -8,15 +8,13 @@ document.addEventListener("DOMContentLoaded", () => {
         { text: "Flag Week Quiz!", url: "https://www.japan.travel/" }
     ];
 
-    // Populate cultural links
     cultureLinks.forEach(link => {
         let li = document.createElement("li");
         let a = document.createElement("a");
         a.href = link.url;
         a.textDecoration = "underline";
         a.textContent = link.text;
-        a.target = "_blank";
-        a.style.textDecoration = "none";
+        a.style.textDecoration = "underline";
         li.appendChild(a);
         linksList.appendChild(li);
     });
