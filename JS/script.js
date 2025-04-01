c9728e83071817c4011f40c07a5f2053ef80bfbd
Patrick <gcc-flagday@gmail.com>
2025-04-01 21:41:04 +0100

    Changed everything, broke everything, went back

--- JS/script.js
+++ JS/script.js
@@ -1,20 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const countryList = document.getElementById("country-list");
     const linksList = document.getElementById("links-list");
 
     const cultureLinks = [
         { text: "New GCC Anthem!", url: "anthem.html" },
         { text: "GCC Flag Redesign!", url: "flag_redesign.html" },
-        { text: "Flag Week Quiz!", url: "quiz.html" }
+        { text: "National Flag History", url: "flag_history.html" },
+        { text: "Flag Week Calendar", url: "calendar.html" },
+        { text: "Flag Week Itinerary", url: "events.html" },
     ];
 
     cultureLinks.forEach(link => {
         let li = document.createElement("li");
         let a = document.createElement("a");
         a.href = link.url;
-        a.textDecoration = "underline";
         a.textContent = link.text;
-        a.style.textDecoration = "underline";
         li.appendChild(a);
         linksList.appendChild(li);
     });
