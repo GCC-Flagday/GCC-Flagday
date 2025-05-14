e24f815076387160782633df6b5eb5b75cd1644a
Patrick <gcc-flagday@gmail.com>
2025-05-14 18:45:55 +0100

    Update (Correction and Removal of invalid Information)

--- JS/script.js
+++ JS/script.js
@@ -3,8 +3,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const cultureLinks = [
         { text: "22 - 24 Flag Day Gallery", url: "gallery.html"},
-        { text: "New GCC Anthem!", url: "anthem.html" },
-        { text: "GCC Flag Redesign!", url: "flag_redesign.html" },
         { text: "National Flag History", url: "flag_history.html" },
         { text: "Flag Week Calendar", url: "calendar.html" },
         { text: "Flag Week Itinerary", url: "events.html" },
@@ -22,16 +20,4 @@ document.addEventListener("DOMContentLoaded", () => {
         li.appendChild(a);
         linksList.appendChild(li);
     });
-});
-
-let logo = document.querySelector(".logo"); 
-let clickCount = 0;
-
-logo.addEventListener("click", () => {
-    clickCount++;
-
-    if (clickCount === 5) {
-        logo.src = "JS/images/jason.jpg"; 
-        clickCount = 0;
-    }
 });
